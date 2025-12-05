46f450baa6f55c93c6c6f698e19a3ecbae76816b
SeoHee3478 <seohee3478@gmail.com>
2025-12-05 21:45:10 +0900

    [chore] 절대경로 및 이미지 url 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "www.culture.go.kr",
+        pathname: "/upload/**",
+      },
+      {
+        protocol: "http",
+        hostname: "www.kopis.or.kr",
+        pathname: "/upload/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
